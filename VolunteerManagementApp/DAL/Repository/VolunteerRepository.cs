3558d6d905ba608f1e32a2732a6d9dcd868cb966
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-27 13:19:29 +0530

    Add files via upload

---
+++ VolunteerManagementApp/DAL/Repository/VolunteerRepository.cs
@@ -0,0 +1,50 @@
+﻿using VolunteerManagementApp.DAL.Interface;
+using VolunteerManagementApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace VolunteerManagementApp.DAL.Repository
+{
+    public class VolunteerRepository : IVolunteerRepository
+    {
+        private VolunteerDbContext _context;
+        public VolunteerRepository(VolunteerDbContext Context)
+        {
+            this._context = Context;
+        }
+      
+        public Volunteer GetVolunteerById(int volunteerId)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Volunteer> GetAllVolunteers()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Volunteer AddVolunteer(Volunteer volunteer)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+
+        public Volunteer UpdateVolunteer(Volunteer updatedVolunteer)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Volunteer DeleteVolunteer(int volunteerId)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VolunteerManagementApp/DAL/Repository/VolunteerService.cs
@@ -0,0 +1,46 @@
+﻿using VolunteerManagementApp.DAL.Interface;
+using VolunteerManagementApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace VolunteerManagementApp.DAL.Repository
+{
+    public class VolunteerManagementService : IVolunteerInterface
+    {
+        private IVolunteerRepository _repo;
+        public VolunteerManagementService(IVolunteerRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        public Volunteer AddVolunteer(Volunteer volunteer)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Volunteer DeleteVolunteer(int volunteerId)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Volunteer> GetAllVolunteers()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Volunteer GetVolunteerById(int volunteerId)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Volunteer UpdateVolunteer(Volunteer volunteer)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
